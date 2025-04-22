4ab8d887998472c28adb61bc17957938831c3b83
raj.kadam <merajkadam@gmail.com>
2025-04-22 20:03:27 +0530

    day 3 push

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from './db/index.js';
 import 'dotenv/config'
+import { app } from "./app.js";
 
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("mongodb connection failed", err)
+})
 
 
 
