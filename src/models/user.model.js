88df305b85e588e6489f454fca3f296d6f1c30b4
raj.kadam <merajkadam@gmail.com>
2025-04-27 20:02:26 +0530

    registration controller done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
 const userSchema = new Schema({
-    username: {
+    userName: {
         type: String,
         required: true,
         unique: true,
@@ -20,7 +20,7 @@ const userSchema = new Schema({
         trim: true,
     },
 
-    fullname: {
+    fullName: {
         type: String,
         required: true,
         trim: true,
@@ -52,7 +52,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -66,8 +66,8 @@ userSchema.methods.generateAccessToken = function () {
         {
             _id: this._id,
             email: this.email,
-            username: this.username,
-            fullname: this.fullname
+            userName: this.username,
+            fullName: this.fullname
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
